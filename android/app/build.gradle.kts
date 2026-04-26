de75692f4def7461cab7467fc421797a941be752
Shreeya-Narayanan <shreeya1125@gmail.com>
2026-04-27 01:22:47 +0530

    Firebase setup + Gradle fixes + SHA1 config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,8 +37,11 @@ android {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 flutter {
     source = "../.."
 }
+
